622aa6785169f2d3c38fd1efce5c831a728fb742
DiegoArceLarios <112732312+DiegoArceLarios@users.noreply.github.com>
2024-07-17 22:48:27 +0300

    Add files via upload

---
+++ options.js
@@ -0,0 +1,20 @@
+var buttom = document.getElementById('box')
+buttom.addEventListener('touchstart', ()=>{
+    var color = document.getElementById("box").style.backgroundColor
+    if(color === 'azure'){
+        document.getElementById("box").style.backgroundColor = 'blue'
+    }else{
+        document.getElementById("box").style.backgroundColor = 'azure'
+    }
+})
+buttom.addEventListener('mousedown', ()=>{
+    var color = document.getElementById("box").style.backgroundColor
+    console.log(color)
+    if(color === 'blue'){
+        document.getElementById("box").style.backgroundColor = 'azure'
+    }else{
+        document.getElementById("box").style.backgroundColor = 'blue'
+    }
+})
+
+console.log(buttom)
